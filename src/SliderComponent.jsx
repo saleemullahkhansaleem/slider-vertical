e7c0bda62cae404f036050e722b5c96f73f73624
Saleemullah Khan Saleem <142225115+saleemullahkhansaleem@users.noreply.github.com>
2024-03-29 14:16:14 +0500

    image cut behavior set

--- src/SliderComponent.jsx
+++ src/SliderComponent.jsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { FaArrowAltCircleDown, FaArrowAltCircleUp } from "react-icons/fa";
 import "./style.css";
 
-const SliderComponent = () => {
-  const slides = [
-    {
-      image:
-        "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-2.jpg",
-      text: "Canyon",
-    },
-    {
-      image:
-        "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-3.jpg",
-      text: "Desert",
-    },
-    {
-      image:
-        "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-4.jpg",
-      text: "Erosion",
-    },
-    {
-      image:
-        "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-1.jpg",
-      text: "Shape",
-    },
-  ];
+const slides = [
+  {
+    image:
+      "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-2.jpg",
+    text: "Canyon",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-3.jpg",
+    text: "Desert",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-4.jpg",
+    text: "Erosion",
+  },
+  {
+    image:
+      "https://raw.githubusercontent.com/supahfunk/supah-codepen/master/canyon-1.jpg",
+    text: "Shape",
+  },
+];
 
+const SliderComponent = () => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isTransitioning, setIsTransitioning] = useState(false);
 
   const nextSlide = () => {
     setActiveIndex((prevIndex) =>
@@ -44,10 +45,8 @@ const SliderComponent = () => {
     setActiveIndex(index);
   };
 
-  const [isTransitioning, setIsTransitioning] = useState(false);
-
   const handleWheel = (event) => {
-    if (isTransitioning) return; // Exit if a transition is in progress
+    if (isTransitioning) return;
 
     if (event.deltaY < 0) {
       prevSlide();
@@ -55,23 +54,62 @@ const SliderComponent = () => {
       nextSlide();
     }
 
-    setIsTransitioning(true); // Set transition flag
-    setTimeout(() => setIsTransitioning(false), 1000); // Reset transition flag after 1 second (adjust as needed)
+    setIsTransitioning(true);
+    setTimeout(() => setIsTransitioning(false), 600);
   };
 
   return (
-    <div onWheel={handleWheel}>
+    <div className="slider-container" onWheel={handleWheel}>
       <div className="slideshow">
-        <div
-          className="slider"
-          style={{ transform: `translateY(-${activeIndex * 100}vh)` }}
-        >
+        <div className="slider">
           {slides.map((slide, index) => (
-            <div
-              className={index === activeIndex ? "item active" : "item"}
-              key={index}
-            >
-              <img src={slide.image} alt={`Slide ${index}`} />
+            <div key={index}>
+              <div
+                className={`slide left ${
+                  index === activeIndex ||
+                  index === activeIndex - 1 ||
+                  (index === slides.length - 1 && activeIndex === 0)
+                    ? "active"
+                    : ""
+                } ${
+                  index === activeIndex - 1 ||
+                  (index === slides.length - 1 && activeIndex === 0)
+                    ? "previous"
+                    : ""
+                } ${
+                  index === activeIndex + 1 ||
+                  (index === 0 && activeIndex === slides.length - 1)
+                    ? "next"
+                    : ""
+                }`}
+              >
+                <div className="item">
+                  <img src={slide.image} alt={`Slide ${index}`} />
+                </div>
+              </div>
+              <div
+                className={`slide right ${
+                  index === activeIndex ||
+                  index === activeIndex - 1 ||
+                  (index === slides.length - 1 && activeIndex === 0)
+                    ? "active"
+                    : ""
+                } ${
+                  index === activeIndex - 1 ||
+                  (index === slides.length - 1 && activeIndex === 0)
+                    ? "previous"
+                    : ""
+                } ${
+                  index === activeIndex + 1 ||
+                  (index === 0 && activeIndex === slides.length - 1)
+                    ? "next"
+                    : ""
+                }`}
+              >
+                <div className="item">
+                  <img src={slide.image} alt={`Slide ${index}`} />
+                </div>
+              </div>
             </div>
           ))}
         </div>
@@ -79,17 +117,17 @@ const SliderComponent = () => {
       <div className="slideshow-text">
         {slides.map((slide, index) => (
           <div
-            className={
-              index === activeIndex
-                ? "item active"
-                : index === activeIndex - 1 ||
-                  (activeIndex === 0 && index === slides.length - 1)
-                ? "item previous"
-                : index === activeIndex + 1 ||
-                  (activeIndex === slides.length - 1 && index === 0)
-                ? "item next"
-                : "item"
-            }
+            className={`item ${index === activeIndex ? "active" : ""} ${
+              index === activeIndex - 1 ||
+              (index === slides.length - 1 && activeIndex === 0)
+                ? "previous"
+                : ""
+            } ${
+              index === activeIndex + 1 ||
+              (index === 0 && activeIndex === slides.length - 1)
+                ? "next"
+                : ""
+            }`}
             key={index}
           >
             {slide.text}
@@ -99,7 +137,7 @@ const SliderComponent = () => {
       <div className="dots">
         {slides.map((slide, index) => (
           <div
-            className={index === activeIndex ? "dot-line active" : "dot-line"}
+            className={`dot-line ${index === activeIndex ? "active" : ""}`}
             onClick={() => handleDotClick(index)}
             key={index}
           ></div>
